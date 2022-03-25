6060b418596386a8e90071a3817d041d4c908495
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-25 12:32:54 +0100

    ciudades

--- Clases/edificio.py
+++ Clases/edificio.py
@@ -1,1 +1,16 @@
 # al día siguiente:
+class Edificio():
+    pass
+
+class Empleado():
+    pass
+
+class Empresa(): # 
+    pass
+
+class Ciudad():
+    def NY():
+        global A, B
+
+    def LA():
+        global C
