6968bcf219882b31f62a2f67376350f2682add60
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-28 18:45:26 +0200

    elimino

--- Clases/edificio.py
+++ Clases/edificio.py
@@ -1,9 +1,2 @@
 # al día siguiente:
 
-ciudad = str(input("¿Qué ciudad quiere que sea destrozada, NY / LA? "))
-edificios = ["A", "B", "C"] # añadir destruidos
-empleados = ["Martin", "Salim", "Sra. Xing"] #añadir muertos
-class Edificio():
-    def __init__(self, nombre, numero_plantas) -> None:
-        self.n = nombre
-        self.plantas = numero_plantas
