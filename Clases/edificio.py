92f2fc1a2e28007474136053b755c0747a23e294
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-28 19:11:59 +0200

    append

--- Clases/edificio.py
+++ Clases/edificio.py
@@ -1,2 +1,29 @@
 # al día siguiente:
 
+from numpy import append
+
+
+class empresa():
+    def __init__(self, nombre) -> None: #constructor
+        self.nombre = nombre
+
+
+class edificio():
+    pass
+
+class ciudad():
+
+
+    pass
+
+class empleados():
+    def __init__(self, nombre) -> None:
+        self.nombre = nombre
+    pass
+
+emp = empresa("YooHoo")
+lista_empleados = ["Martin", "Salim"]
+lista = []
+for nombre in lista_empleados:
+    empleado = empleados(nombre)
+    lista.append(empleado)
