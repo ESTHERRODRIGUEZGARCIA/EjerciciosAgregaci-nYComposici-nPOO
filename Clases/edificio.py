8561dcafecbccaeba99aae076e83d2b9896a61bd
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-28 18:44:59 +0200

    empiezo again :(((((

--- Clases/edificio.py
+++ Clases/edificio.py
@@ -7,25 +7,3 @@ class Edificio():
     def __init__(self, nombre, numero_plantas) -> None:
         self.n = nombre
         self.plantas = numero_plantas
-class Empleado():
-    pass
-
-class Empresa(): #
-    pass
-
-class Ciudad():
-    pass
-
-class NewYork():
-    def __del__(self):
-        print("Destrucción edificio de Nueva York.")
-
-class LosAngeles():
-    def __del__(self):
-        print("Destrucción edificio de LosÁngeles.")
-
-
-edificio = Edificio("hola", 23)
-edificio.getNombre()
-edificio.n = "res"
-edificio.setNombre("res")
